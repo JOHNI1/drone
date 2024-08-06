80bbd0b66c807428d45c1382aa09b4c9f7b411a9
johni <takahashiyoni@gmail.com>
2024-08-06 15:13:43 +0300

    changed gazebo_classic_sim.launch.py

--- launch/gazebo_classic_sim.launch.py
+++ launch/gazebo_classic_sim.launch.py
@@ -3,9 +3,8 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 from launch.actions import OpaqueFunction
 import xacro
 
@@ -49,9 +48,8 @@ def initializer(context):
             output='screen'
         ),
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [PathJoinSubstitution([FindPackageShare('gazebo_ros'), 'launch', 'gazebo.launch.py'])]
-            ),
+            PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+            launch_arguments={'verbose': 'true'}.items(),
         ),
         Node(
             package='gazebo_ros',
